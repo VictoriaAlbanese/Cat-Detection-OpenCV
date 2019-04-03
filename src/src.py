c86ee0675cb9d18b21d8c5d9549a6c889f0537d4
Hannah Chiodo <hannah.chiodo@gmail.com>
2019-04-03 13:34:35 -0400

    Add preprocessing and sift file

---
+++ src/src.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+from glob import glob
+import numpy as np
+
+dataPath = os.getcwd() + "\\existing_detector\\"
+imgs = [f for f in glob(dataPath + 'images\\*') if '.jpg' in f]
+i = 1
+for f in imgs:
+    image = cv2.imread(f, -1)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp = sift.detect(gray, None)
+
+    img = cv2.drawKeypoints(gray, kp, image)
+    cv2.imwrite('src/sift_imgs/sift_keypoints{0}.jpg'.format(i), img)
+    i += 1
